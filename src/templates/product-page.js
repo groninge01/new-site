74d10bee3c823dca6148bcc28d5bb8b4f2ac8d23
Stefan <groninge@hotmail.com>
2019-03-25 20:16:19 +0100

    Add heading here too

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -178,13 +178,7 @@ export const productPageQuery = graphql`
         description
         intro {
           blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
+            heading
             text
           }
           heading
