a1d88553f85a94d8d58b0909091bac26ee0472f2
Stefan <groninge@hotmail.com>
2019-03-26 20:04:31 +0100

    Create feature tiles

--- src/components/Features.js
+++ src/components/Features.js
@@ -4,15 +4,13 @@ import PropTypes from 'prop-types'
 const FeatureGrid = ({ gridItems }) => (
   <div className="columns is-multiline">
     {gridItems.map(item => (
-      <div key={item.text} className="column is-6">
-        <section className="section">
-        <div className="has-text-centered">
-          <h2
-          className="first-letter"
-          >{item.heading}</h2>
-          </div>
-          <p>{item.text}</p>
-        </section>
+      <div key={item.text} className="is-parent column is-6">
+        <div className="tile is-child box notification">
+          <section className="section">
+            <h2 className="first-letter">{item.heading}</h2>
+            <p>{item.text}</p>
+          </section>
+        </div>
       </div>
     ))}
   </div>
